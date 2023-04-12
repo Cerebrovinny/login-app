0af88d3f497af727307bc30d71107763be3a098c
Cerebrovinny <vinny.cardoso96@gmail.com>
2023-04-12 11:46:09 -0300

    added user models

---
+++ backend/models/user.go
@@ -0,0 +1,26 @@
+package models
+
+import (
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type Credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type User struct {
+	Username string `bson:"username"`
+	Password string `bson:"password"`
+}
+
+// CheckPassword compares a hashed password with a plaintext password and returns an error if they don't match.
+func CheckPassword(hashedPassword, password string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	if err != nil {
+		return errors.New("invalid password")
+	}
+	return nil
+}
