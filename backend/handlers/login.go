8fde4cbc9deccd86b530ddec24e16e8d0d24864b
Cerebrovinny <vinny.cardoso96@gmail.com>
2023-04-12 13:25:05 -0300

    added user handlers

---
+++ backend/handlers/login.go
@@ -0,0 +1,94 @@
+package handlers
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/Cerebrovinny/login-app/config"
+	"github.com/Cerebrovinny/login-app/models"
+	"github.com/dgrijalva/jwt-go"
+	"go.mongodb.org/mongo-driver/bson"
+	"net/http"
+	"time"
+)
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+var jwtKey = []byte("env")
+
+func getUser(username string) (models.User, error) {
+	db, err := config.GetDatabase()
+	if err != nil {
+		return models.User{}, err
+	}
+
+	client, err := config.GetMongoClient()
+	if err != nil {
+		return models.User{}, err
+	}
+	defer client.Disconnect(context.Background())
+
+	collection := db.Collection("users")
+	var user models.User
+	err = collection.FindOne(context.Background(), bson.M{"username": username}).Decode(&user)
+	return user, err
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		json.NewEncoder(w).Encode(map[string]string{"message": "Method not allowed"})
+		return
+	}
+
+	var creds models.Credentials
+
+	err := json.NewDecoder(r.Body).Decode(&creds)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"message": "Invalid request payload"})
+		return
+	}
+
+	user, err := getUser(creds.Username)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(map[string]string{"message": "Invalid username or password"})
+		return
+	}
+
+	err = models.CheckPassword(user.Password, creds.Password)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(map[string]string{"message": "Invalid username or password"})
+		return
+	}
+
+	expirationTime := time.Now().Add(5 * time.Minute)
+
+	claims := &Claims{
+		Username: creds.Username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"message": "Error generating token"})
+		return
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:    "token",
+		Value:   tokenString,
+		Expires: expirationTime,
+	})
+
+	json.NewEncoder(w).Encode(map[string]string{"message": "Login successful!"})
+}
